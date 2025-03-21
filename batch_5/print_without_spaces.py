5e90b929ecbe506d8a04301f8bb5998b7c06c701
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-21 22:02:40 +0800

    revised pseudocode

--- batch_5/print_without_spaces.py
+++ batch_5/print_without_spaces.py
@@ -3,7 +3,7 @@
 # Input:         Juan Dela Cruz
 # Output: Juan Dela Cruz
 
-# Ask user yo input their fullname, add several space characters at the beginning
+# Ask user to input their fullname, add several space characters at the beginning
 full_name = input("Enter your full name (add spaces before input): ")
 
 # Print input without the spaces in the beginning
