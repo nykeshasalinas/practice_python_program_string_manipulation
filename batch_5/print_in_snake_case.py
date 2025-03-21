db3bb3a5123a9b911af4e3950ca6d4e63642349f
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-21 23:12:19 +0800

    file creation

---
+++ batch_5/print_in_snake_case.py
@@ -0,0 +1,4 @@
+# Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: juan_dela_cruz
