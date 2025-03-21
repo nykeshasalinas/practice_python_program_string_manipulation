32b8e5001f3847079cd30ff7fd6fd9a49e0dccd4
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-21 22:49:40 +0800

    file creation

---
+++ batch_5/print_number_of_words.py
@@ -0,0 +1,4 @@
+# Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+# Example:
+# Input: We will weather the weather whatever the weather whether we like it or not
+# Output: 14
