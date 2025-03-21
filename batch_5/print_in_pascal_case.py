56e1922dafe3f912217e93b2ba98add1d78dbc5e
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-21 23:00:44 +0800

    file creation

---
+++ batch_5/print_in_pascal_case.py
@@ -0,0 +1,4 @@
+# Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: JuanDelaCruz
