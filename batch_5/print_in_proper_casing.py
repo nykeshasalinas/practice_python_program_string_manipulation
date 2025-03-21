6660d31b3478ba0009329c95b18f5d69fda24b08
nykeshasalinas <salinasnykesha12@gmail.com>
2025-03-21 22:28:36 +0800

    file creation

---
+++ batch_5/print_in_proper_casing.py
@@ -0,0 +1,4 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: Juan Dela Cruz
